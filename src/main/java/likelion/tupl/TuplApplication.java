f870b986f30df9f51b80135b75e253efbb5ef43a
iamneveralone <fieldsawarded@naver.com>
2023-11-08 19:19:41 +0900

    feat : project init

---
+++ src/main/java/likelion/tupl/TuplApplication.java
@@ -0,0 +1,13 @@
+package likelion.tupl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuplApplication.class, args);
+	}
+
+}
