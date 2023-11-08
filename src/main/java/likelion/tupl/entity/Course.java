5035109f5bcb2f7ef78c0e8431ebef06ef88be12
Als-ET <et_oh@naver.com>
2023-11-08 20:57:19 +0900

    feat : Course 엔티티 정보 추가

--- src/main/java/likelion/tupl/entity/Course.java
+++ src/main/java/likelion/tupl/entity/Course.java
@@ -1,5 +1,26 @@
 package likelion.tupl.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Course {
-    // commit test
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String color;
+    private String stu_name;
+    private Integer stu_age;
+    private Integer stu_grade;
+    private String parent_phone;
+    private String subject;
+    private String course_time;
+    private Integer payment_cycle;
+    private Boolean want_alarmtalk;
 }
