d95a0cce0c00772bd6c9783a45eda1fce4244519
13155a1 <ppy040@gmail.com>
2023-11-11 17:36:13 +0900

    feat: Create lastHomeworkList

--- src/main/java/likelion/tupl/repository/HomeworkRepository.java
+++ src/main/java/likelion/tupl/repository/HomeworkRepository.java
@@ -4,6 +4,9 @@ import likelion.tupl.entity.Homework;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HomeworkRepository extends JpaRepository<Homework, Long> {
+    List<Homework> findByLessonId(Long lessonId);
 }
