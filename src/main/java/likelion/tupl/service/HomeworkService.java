3a1f42e2cf209358eaeb3aa2540bd9f63244bb78
13155a1 <ppy040@gmail.com>
2023-11-11 15:16:44 +0900

    feat: Create deleteHomework

--- src/main/java/likelion/tupl/service/HomeworkService.java
+++ src/main/java/likelion/tupl/service/HomeworkService.java
@@ -2,12 +2,17 @@ package likelion.tupl.service;
 
 import likelion.tupl.dto.HomeworkDto;
 import likelion.tupl.entity.Homework;
+import likelion.tupl.exception.ResourceNotFoundException;
 import likelion.tupl.repository.HomeworkRepository;
 import likelion.tupl.repository.LessonRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +20,7 @@ public class HomeworkService {
     private final HomeworkRepository homeworkRepository;
     private final LessonRepository lessonRepository;
 
+    // create homework: course_id에 대한 숙제 생성
     public HomeworkDto createHomework(Long lesson_id, HomeworkDto homeworkDto) {
         // DB에 저장할 Homework 객체 생성
         Homework homework = new Homework();
@@ -37,5 +43,16 @@ public class HomeworkService {
         homeworkDto.setLessonId(homework.getLesson().getId());
 
         return homeworkDto;
+    }
+
+    // delete homework: homework_id에 대한 숙제 삭제
+    public ResponseEntity<Map<String, Boolean>> deleteHomework(Long homework_id) {
+        // 삭제할 Homework 객체 가져옴
+        Homework homework = homeworkRepository.findById(homework_id)
+                .orElseThrow(() -> new ResourceNotFoundException("Homework not exist with id :" + homework_id));
+        homeworkRepository.delete(homework);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
