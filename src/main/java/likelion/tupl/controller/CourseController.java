982a66f1705f6961b6c1f52ca3b209048d3f63ea
Als-ET <et_oh@naver.com>
2023-11-12 18:52:06 +0900

    feat : CourseController /api 삭제

--- src/main/java/likelion/tupl/controller/CourseController.java
+++ src/main/java/likelion/tupl/controller/CourseController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+
 public class CourseController {
     final private CourseService courseService;
 
